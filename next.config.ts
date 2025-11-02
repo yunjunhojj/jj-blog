0d54ec96d1e9598d3d7d4867f4cb79ee7d89c8c0
yunjunho <yunjunhojj@gmail.com>
2025-11-03 00:20:31 +0900

    feat: add SVG loader configuration for turbopack

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,34 @@ import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: "preset-default",
+                    params: {
+                      overrides: {
+                        // customize default plugin options
+                        removeViewBox: false,
+                      },
+                    },
+                  },
+                  "removeDimensions",
+                ],
+              },
+            },
+          },
+        ],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 const withMDX = createMDX({
